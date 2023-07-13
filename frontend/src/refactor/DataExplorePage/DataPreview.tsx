e07c2886c11efa92f151190061e341f5cac22e6e
Yen-Chia Chen <107033180+YenChiaChen@users.noreply.github.com>
2023-07-13 14:51:14 +0800

    feat: add the collapse view

--- frontend/src/refactor/DataExplorePage/DataPreview.tsx
+++ frontend/src/refactor/DataExplorePage/DataPreview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGetDatasetPreviewQuery } from '../../api';
 
 interface DatasetPreviewProps {
@@ -12,32 +12,33 @@ const DatasetPreview: React.FC<DatasetPreviewProps> = ({ filename }) => {
   if (error) {console.log(error); return <p>Something's wrong...</p>};
 
   return (
-    <div className='p-5 bg-white rounded-xl shadow-xl'>
-        <p className='text-sm text-[#333333] my-5'>Preview</p>
-<div className="overflow-auto h-[300px] w-[800px]">  {/* Adjust the height value to what you need */}
-  <table className="table table-xs table-auto min-w-full table-pin-rows">
-    <thead>
-      <tr>
-        {/* Generate the header row based on the keys of the first item */}
-        {Object.keys(previewData[0]).map((key) => (
-          <th key={key}>{key}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {/* Generate the data rows */}
-      {previewData.map((item: Record<string, any>, index: number) => (
-        <tr key={index}>
-          {Object.values(item).map((value: any, i: number) => (
-            <td key={i}>{String(value)}</td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-</div>
-
+    <div tabIndex={0} className="collapse collapse-plus border border-base-300 shadow">
+  <div className="collapse-title text-sm font-medium">
+    Preview
+  </div>
+  <div className="collapse-content"> 
+  <div className="overflow-auto h-[300px] w-[800px] my-10">
+          <table className="table table-xs table-auto min-w-full table-pin-rows">
+            <thead>
+              <tr>
+                {Object.keys(previewData[0]).map((key) => (
+                  <th key={key}>{key}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {previewData.map((item: Record<string, any>, index: number) => (
+                <tr key={index}>
+                  {Object.values(item).map((value: any, i: number) => (
+                    <td key={i}>{String(value)}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+  </div>
+  </div>
   );
 };
 
